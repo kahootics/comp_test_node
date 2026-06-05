cffa3ec4a8599c47ed236705b45ad24e49273cb1
kahootics <araldo.del.kaos@gmail.com>
2026-06-05 18:10:00 +0200

    test deploy

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+
+import fs from 'node:fs';
+import path from 'node:path';
+import { glob } from 'glob';
+
+if (fs.existsSync('./dist')) {
+    fs.rmSync('./dist', { recursive: true });
+}
+
+const dest = './dist/json.json';
+
+function getFileName() {}
+
+const scriptFiles = await glob('assets/**/*.js', { cwd: './dist' }); // GOOD
+
+const out: { [key: string]: string }[] = [];
+scriptFiles.forEach(script => {
+    const key = path.basename(script, '.js');
+    out.push({
+        [key]: script
+    });
+})
+
+const outPath = path.resolve(dest);
+
+  		fs.mkdirSync(path.dirname(outPath), { recursive: true });
+
+    	
+  		fs.writeFileSync(
+    		outPath,
+			JSON.stringify(out),
+    		'utf-8'
+		)
+
+        console.log(`File written at: ${outPath} (${fs.statSync(outPath).size} bytes)`);
