14ef3d8cf7ac230c2b985fe12e88466b5b203afb
kahootics <araldo.del.kaos@gmail.com>
2026-06-15 16:18:15 +0200

    Fix urls

--- src/config/companion-util.ts
+++ src/config/companion-util.ts
@@ -8,6 +8,7 @@ import config from './companion.config.json' with {type: 'json'};
  */
 export function toPublicUrl(filePath: string): string {
     // take anything after dist/
-    const relative = filePath.split('dist/')[1];
-    return `${config.site + config.base}/${relative}`;
+    return filePath.split('dist/')[1] ?? filePath;
+   // return `${config.site + config.base}/${relative}`;
 }
+
